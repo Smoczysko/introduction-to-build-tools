4071278e73bdbec44d2a58c18e3c93dfb14c3423
lukasz.rybka@solwit.pl <lukasz.rybka@solwit.pl>
2014-11-26 22:49:30 +0100

    Maven JEE 6 Web Profile example with Jetty plugin.

---
+++ 04-maven-jee-web-profile/src/main/java/pl/org/dragonia/HelloServlet.java
@@ -0,0 +1,21 @@
+package pl.org.dragonia;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html><body><h2>Hello from servlet!</h2></body></html>");
+        writer.close();
+    }
+}
